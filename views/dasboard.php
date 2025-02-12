eff61297086ecdf360cad0ad1cb9bc2797c0656b
puranoide <acostasanchezangelgabriel@gmail.com>
2025-02-12 10:37:06 -0500

    Retoque v1

--- views/dasboard.php
+++ views/dasboard.php
@@ -29,102 +29,199 @@ if (!isset($_SESSION['nombre'])) {
         <div class="container">
             <form action="" method="post">
                 <h1>FACEBOOK</h1>
-                <label for="email">Seguidores totales</label>
-                <input type="text" name="seguidoresfb" id="seguidoresfb">
-                <label for="email"># seguidores del mes</label>
-                <input type="text" name="seguidoresdelmesfb" id="seguidoresdelmesfb">
-                <label for="email"># Posts (cuántas en el mes)</label>
-                <input type="text" name="postalmesfb" id="postalmesfb">
-                <label for="email">Visualizaciones</label>
-                <input type="text" name="visualizacionesfb" id="visualizacionesfb">
-                <label for="email">Alcance</label>
-                <input type="text" name="alcancefb" id="alcancefb">
-                <label for="email">Interacciones con el contenido</label>
-                <input type="text" name="interracionesconelcontenidofb" id="interracionesconelcontenidofb">
-                <label for="email">Clics en enlace</label>
-                <input type="text" name="clicsenelalcancefb" id="clicsenelalcancefb" placeholder="Agregar dato del mismo mes 2024">
-                <label for="email">Inversión Publicitaria</label>
-                <input type="text" name="inversionPublicitaria" id="inversionPublicitaria">
-                <label for="email"># Publicaciones</label>
-                <input type="text" name="nPublicacionesfb" id="nPublicacionesfb">
-                <label for="email">Visualizaciones</label>
-                <input type="text" name="visualizacionesxpublicacionesfb" id="visualizacionesxpublicacionesfb">
-                <label for="email">Interacciones</label>
-                <input type="text" name="interaccionesxpublicacionesfb" id="interaccionesxpublicacionesfb">
+                <div class="fila">
+                    <label for="email">Seguidores totales</label>
+                    <input type="text" name="seguidoresfb" id="seguidoresfb">
+                </div>
+                <div class="fila">
+                    <label for="email"># seguidores del mes</label>
+                    <input type="text" name="seguidoresdelmesfb" id="seguidoresdelmesfb">
+                    
+                </div>
+                <div class="fila">
+                    <label for="email"># Posts (cuántas en el mes)</label>
+                    <input type="text" name="postalmesfb" id="postalmesfb">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones</label>
+                    <input type="text" name="visualizacionesfb" id="visualizacionesfb">
+                </div>
+                <div class="fila">
+                    <label for="email">Alcance</label>
+                    <input type="text" name="alcancefb" id="alcancefb">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones con el contenido</label>
+                    <input type="text" name="interracionesconelcontenidofb" id="interracionesconelcontenidofb">
+                </div>
 
-                <label for="email"># Reels </label>
-                <input type="text" name="nReels" id="nReels">
+                <div class="fila">
+                    <label for="email">Clics en enlace</label>
+                    <input type="text" name="clicsenelalcancefb" id="clicsenelalcancefb" placeholder="Agregar dato del mismo mes 2024">
+                </div>
+                <div class="fila">
+                    <label for="email">Inversión Publicitaria</label>
+                    <input type="text" name="inversionPublicitaria" id="inversionPublicitaria">
+                </div>
+                <div class="fila">
+                    <label for="email"># Publicaciones</label>
+                    <input type="text" name="nPublicacionesfb" id="nPublicacionesfb">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones</label>
+                    <input type="text" name="visualizacionesxpublicacionesfb" id="visualizacionesxpublicacionesfb">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones</label>
+                    <input type="text" name="interaccionesxpublicacionesfb" id="interaccionesxpublicacionesfb">
+                </div>
 
-                <label for="email"># Visualizaciones </label>
-                <input type="text" name="visualizacionesxreels" id="visualizacionesxreels">
-                <label for="email">Interacciones </label>
-                <input type="text" name="interaccionesxreels" id="interaccionesxreels">
+                <div class="fila">
+                    <label for="email"># Reels </label>
+                    <input type="text" name="nReels" id="nReels">
 
-                <label for="email"># Historias </label>
-                <input type="text" name="nHistorias" id="nHistorias">
-
-                <label for="email">Visualizaciones </label>
-                <input type="text" name="visualizacionesxhistorias" id="visualizacionesxhistorias">
-                <label for="email">Interacciones </label>
-                <input type="text" name="interaccionesxhistorias" id="interaccionesxhistorias">
+                </div>
+                <div class="fila">
+                    <label for="email"># Visualizaciones </label>
+                    <input type="text" name="visualizacionesxreels" id="visualizacionesxreels">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones </label>
+                    <input type="text" name="interaccionesxreels" id="interaccionesxreels">
+                </div>
+                <div class="fila">
+                    <label for="email"># Historias </label>
+                    <input type="text" name="nHistorias" id="nHistorias">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones </label>
+                    <input type="text" name="visualizacionesxhistorias" id="visualizacionesxhistorias">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones </label>
+                    <input type="text" name="interaccionesxhistorias" id="interaccionesxhistorias">
+                </div>
 
                 <h1>INSTAGRAM</h1>
-                <label for="email">Seguidores totales </label>
-                <input type="text" name="seguidoresTotalesIg" id="seguidoresTotalesIg">
-                <label for="email"># Seguidores del mes</label>
-                <input type="text" name="seguidoresdelmesIg" id="seguidoresdelmesIg">
-                <label for="email"># Posts (cuántas en el mes)</label>
-                <input type="text" name="nPostsEnElMesIg" id="nPostsEnElMesIg">
-                <label for="email">Visualizaciones BFB</label>
-                <input type="text" name="visualizacionesBFB" id="visualizacionesBFB">
-                <label for="email">Alcance</label>
-                <input type="text" name="alcanceIg" id="alcanceIg" placeholder="Agregar dato del mismo mes 2024">
-                <label for="email">Interacciones con el contenido</label>
-                <input type="text" name="interaccionesconelcontenidoig" id="interaccionesconelcontenidoig" placeholder="Agregar dato del mismo mes 2024">
-                <label for="email">Clics en enlace</label>
-                <input type="text" name="clicsenelalcanceig" id="clicsenelalcanceig" placeholder="Agregar dato del mismo mes 2024">
-                <label for="email">Inversión Publicitaria</label>
-                <input type="text" name="inversionPublicitariaig" id="inversionPublicitariaig">
-                <label for="email"># Publicaciones</label>
-                <input type="text" name="nPublicacionesig" id="nPublicacionesig">
-                <label for="email">Visualizaciones</label>
-                <input type="text" name="visualizacionesxpublicacionesig" id="visualizacionesxpublicacionesig">
-                <label for="email">Interacciones</label>
-                <input type="text" name="interaccionesxpublicacionesig" id="interaccionesxpublicacionesig">
-                <label for="email"># Reels </label>
-                <input type="text" name="nReelsig" id="nReelsig">
-                <label for="email">Visualizaciones </label>
-                <input type="text" name="visualizacionesxreelsig" id="visualizacionesxreelsig">
-                <label for="email">Interacciones</label>
-                <input type="text" name="interaccionesxreelsig" id="interaccionesxreelsig">
-                <label for="email"># Historias</label>
-                <input type="text" name="nHistoriasig" id="nHistoriasig">
-                <label for="email">Alcance</label>
-                <input type="text" name="alcanceHistoriasig" id="alcanceHistoriasig">
-                <label for="email">Interacciones</label>
-                <input type="text" name="interaccionesHistoriasig" id="interaccionesHistoriasig">
+
+                <div class="fila">
+                    <label for="email">Seguidores totales </label>
+                    <input type="text" name="seguidoresTotalesIg" id="seguidoresTotalesIg">
+                </div>
+                <div class="fila">
+                    <label for="email"># Seguidores del mes</label>
+                    <input type="text" name="seguidoresdelmesIg" id="seguidoresdelmesIg">
+                </div>
+                <div class="fila">
+                    <label for="email"># Posts (cuántas en el mes)</label>
+                    <input type="text" name="nPostsEnElMesIg" id="nPostsEnElMesIg">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones BFB</label>
+                    <input type="text" name="visualizacionesBFB" id="visualizacionesBFB">
+                </div>
+                <div class="fila">
+                    <label for="email">Alcance</label>
+                    <input type="text" name="alcanceIg" id="alcanceIg" placeholder="Agregar dato del mismo mes 2024">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones con el contenido</label>
+                    <input type="text" name="interaccionesconelcontenidoig" id="interaccionesconelcontenidoig" placeholder="Agregar dato del mismo mes 2024">
+                </div>
+                <div class="fila">
+                    <label for="email">Clics en enlace</label>
+                    <input type="text" name="clicsenelalcanceig" id="clicsenelalcanceig" placeholder="Agregar dato del mismo mes 2024">
+                </div>
+                <div class="fila">
+                    <label for="email">Inversión Publicitaria</label>
+                    <input type="text" name="inversionPublicitariaig" id="inversionPublicitariaig">
+                </div>
+                <div class="fila">
+                    <label for="email"># Publicaciones</label>
+                    <input type="text" name="nPublicacionesig" id="nPublicacionesig">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones</label>
+                    <input type="text" name="visualizacionesxpublicacionesig" id="visualizacionesxpublicacionesig">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones</label>
+                    <input type="text" name="interaccionesxpublicacionesig" id="interaccionesxpublicacionesig">
+                </div>
+                <div class="fila">
+                    <label for="email"># Reels </label>
+                    <input type="text" name="nReelsig" id="nReelsig">
+                </div>
+                <div class="fila">
+                    <label for="email">Visualizaciones </label>
+                    <input type="text" name="visualizacionesxreelsig" id="visualizacionesxreelsig">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones</label>
+                    <input type="text" name="interaccionesxreelsig" id="interaccionesxreelsig">
+                </div>
+                <div class="fila">
+                    <label for="email"># Historias</label>
+                    <input type="text" name="nHistoriasig" id="nHistoriasig">
+                </div>
+                <div class="fila">
+                    <label for="email">Alcance</label>
+                    <input type="text" name="alcanceHistoriasig" id="alcanceHistoriasig">
+                </div>
+                <div class="fila">
+                    <label for="email">Interacciones</label>
+                    <input type="text" name="interaccionesHistoriasig" id="interaccionesHistoriasig">
+                </div>
+
 
                 <h1>TikTok</h1>
-                <label for="email">Seguidores totales</label>
-                <input type="text" name="seguidorestotalesTikTok" id="seguidorestotalesTikTok">
-                <label for="email"># Seguidores del mes</label>
-                <input type="text" name="nSeguidoresDelMesTikTok" id="nSeguidoresDelMesTikTok">
-                <label for="email"># Publicaciones</label>
-                <input type="text" name="nPublicacionesTikTok" id="nPublicacionesTikTok">
-                <label for="email">Me gusta del mes</label>
-                <input type="text" name="nMeGustaDelMesTikTok" id="nMeGustaDelMesTikTok">
-                <label for="email">Visualizaciones de video (máx)</label>
-                <input type="text" name="visualizacionesdevideoTikTok" id="visualizacionesdevideoTikTok">
+
+                <div class="fila">
+                    <label for="email">Seguidores totales</label>
+                    <input type="text" name="seguidorestotalesTikTok" id="seguidorestotalesTikTok">
+                </div>
+                <div class="fila">
+                    <label for="email"># Seguidores del mes</label>
+                    <input type="text" name="nSeguidoresDelMesTikTok" id="nSeguidoresDelMesTikTok">
+                </div>
+                <div class="fila">
+                    <label for="email"># Publicaciones</label>
+                    <input type="text" name="nPublicacionesTikTok" id="nPublicacionesTikTok">
+                </div>
+                <div class="fila">
+                    <label for="email">Me gusta del mes</label>
+                    <input type="text" name="nMeGustaDelMesTikTok" id="nMeGustaDelMesTikTok">
+                </div>
+
+                <div class="fila">
+                    <label for="email">Visualizaciones de video (máx)</label>
+                    <input type="text" name="visualizacionesdevideoTikTok" id="visualizacionesdevideoTikTok">
+                </div>
+
                 <h1>X</h1>
-                <label for="email">Seguidores totales</label>
-                <input type="text" name="seguidorestotalesx" id="seguidorestotalesx">
-                <label for="email"># Seguidores del mes</label>
-                <input type="text" name="nSeguidoresDelMesx" id="nSeguidoresDelMesTikTok">
-                <label for="email">Impresiones</label>
-                <input type="text" name="inpremionesx" id="inpremionesx">
-               
+
+                <div class="fila">
+                    <label for="email">Seguidores totales</label>
+                    <input type="text" name="seguidorestotalesx" id="seguidorestotalesx">
+                </div>
+
+                <div class="fila">
+                    <label for="email"># Seguidores del mes</label>
+                    <input type="text" name="nSeguidoresDelMesx" id="nSeguidoresDelMesTikTok">
+                </div>
+
+                <div class="fila">
+                    <label for="email">Impresiones</label>
+                    <input type="text" name="inpremionesx" id="inpremionesx">
+                </div>
+
+
             </form>
         </div>
+        <div class="container">
+        <h1>REGISTROS</h1>
+        </div>
+
     </main>
 
 </body>
