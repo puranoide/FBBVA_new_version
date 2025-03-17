244d4ccde7d5e773f5dab09390357e57dab338d3
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2025-03-17 12:49:59 -0500

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -6,7 +6,7 @@ var contrasena= document.getElementById("contrasena");
 var message = document.getElementById("message");
 function login(usuario, contrasena) {
 
-    fetch("http://localhost/fbbva_new_version/controller/auth.php", {
+    fetch("../controller/auth.php", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
