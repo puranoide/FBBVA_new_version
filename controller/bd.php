b53f6105202a9ced080d412040b1410f21e17003
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2025-04-10 15:36:59 -0500

    Update bd.php

--- controller/bd.php
+++ controller/bd.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "fbbva_newversion";
+$username = "u685818680_admin_gab";
+$password = "GptYi#R5";
+$database = "u685818680_fbbva_nv";
 $conexion = mysqli_connect($servername,$username,$password,$database);
 
 if (!$conexion) {
