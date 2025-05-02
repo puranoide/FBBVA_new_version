69b03dfa35c47947c62aeb7ee2d4f654a3340bf8
puranoide <121548953+puranoide@users.noreply.github.com>
2025-05-02 11:00:16 -0500

    Update bd.php

--- controller/bd.php
+++ controller/bd.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "u685818680_admin_gab";
-$password = "GptYi#R5";
-$database = "u685818680_fbbva_nv";
-$conexion = mysqli_connect($servername,$username,$password,$database);
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "fbbva";
+$conexion = mysqli_connect($servername,$username,$password,$database);
 
 if (!$conexion) {
     die("Connection failed: " . mysqli_connect_error());
